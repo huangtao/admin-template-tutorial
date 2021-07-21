21e66973caa3c1ea86cf38352542e5a2fb4ee579
huangtao <huangtao117@yeah.net>
2021-07-21 16:00:53 +0800

    初步完成页面设计

---
+++ step-4-asp-net-core/Models/FkPay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootstrapAdmin.Models
+{
+    public class FkPay
+    {
+        public String TradeNo { get; set; }
+        public String UserId { get; set; }
+        public int PayPrice { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+
+    public class FkDayPay
+    {
+        public DateTime CreateDate { get; set; }
+        public int SumPrice { get; set; }
+    }
+}
